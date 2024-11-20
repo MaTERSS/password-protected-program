40d31c54e7d9ed30b05e582267e22be6299a7d33
Ardor <Ardor@DESKTOP-BQF7240>
2024-11-20 17:05:56 +0800

    Добавьте файлы проекта.

---
+++ password protected program/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Создайте переменную типа string, в которой хранится пароль для доступа к тайному сообщению. 
+Пользователь вводит пароль, далее происходит проверка пароля на правильность, и если пароль неверный, то попросите его ввести пароль ещё раз. 
+Если пароль подошёл, выведите секретное сообщение.  
+
+Если пользователь неверно ввел пароль 3 раза, программа завершается.
+*/
+using System;
+using System.Text;
+
+namespace CSharplight
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            string password = "Инчакутек";
+            int trysCount = 3;
+            int trys = 1;            
+
+            for (int i = 0; i < trysCount; i++) 
+            {             
+                Console.WriteLine("Введите пароль:");
+                string inputPassword = Console.ReadLine();
+
+                if (inputPassword == password)
+                {
+                    Console.WriteLine("Секретный шифр:\nЗдесь был гараж...");
+                    break;
+                }
+                if (trysCount - i - trys < 1)
+                {
+                    Console.WriteLine("Неверный пароль - завершение программы...");
+                }
+                else
+                {
+                    Console.WriteLine($"Количество попыток осталось: {trysCount - i - trys}");
+                }
+            }
+        }
+    }
+}
